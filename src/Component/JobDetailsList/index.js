536017323a517b5cea8412cc349fcb5a6e14d2fa
VineelaMuthyala <131747823+VineelaMuthyala@users.noreply.github.com>
2023-10-05 11:26:50 +0530

    Update index.js

--- src/Component/JobDetailsList/index.js
+++ src/Component/JobDetailsList/index.js
@@ -1,8 +1,10 @@
 import {AiFillStar} from 'react-icons/ai'
+import {GoLocation} from 'react-icons/go'
+
 import './index.css'
 
 const JobDetailsList = props => {
-  const {jobDetails} = props
+  const {jobDetailsItem} = props
   const {
     companyLogoUrl,
     employmentType,
@@ -12,20 +14,34 @@ const JobDetailsList = props => {
     packagePerAnnum,
     rating,
     title,
-  } = jobDetails
-
+  } = jobDetailsItem
+  console.log(jobDetailsItem)
   return (
     <div className="job-details-container">
       <div className="logo-title-rating-container">
         <img className="logo" alt="logo" src={companyLogoUrl} />
         <div className="title-rating-container">
-          <h1 className="title-heading">{employmentType}</h1>
+          <h1 className="title-heading">{title}</h1>
           <div className="rating-container">
             <AiFillStar className="star-rating" />
             <p className="rating-text">{rating}</p>
           </div>
         </div>
       </div>
+
+      <div className="location-internship-salary-container">
+        <div className="location-internship-container">
+          <div className="location-container">
+            <GoLocation className="location-icon" />
+            <p className="location-text">{location}</p>
+          </div>
+          <div className="employment-type-container">
+            <p className="employment-type-text">{employmentType}</p>
+          </div>
+        </div>
+        <p className="package-text">{packagePerAnnum}</p>
+      </div>
+      <hr />
     </div>
   )
 }
