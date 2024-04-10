6c62582d2e964979e42201cd964b5bded8600a46
VineelaMuthyala <muthyalavineela@gmail.com>
2024-04-10 06:48:29 +0000

    Jobby App

---
+++ src/Components/HeaderRoute/index.js
@@ -0,0 +1,37 @@
+import {withRouter, Link} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import './index.css'
+
+const HeaderRoute = props => {
+  const onLogout = () => {
+    const {history} = props
+    console.log(history)
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <div className="header-container">
+      <Link to="/" className="link">
+        <img
+          className="header-logo-image"
+          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+          alt="website logo"
+        />
+      </Link>
+      <ul className="options-container">
+        <Link to="/" className="link">
+          <li className="header-text">Home</li>
+        </Link>
+        <Link to="/jobs" className="link">
+          <li className="header-text">Jobs</li>
+        </Link>
+      </ul>
+      <button className="logout-btn" type="button" onClick={onLogout}>
+        Logout
+      </button>
+    </div>
+  )
+}
+
+export default withRouter(HeaderRoute)
